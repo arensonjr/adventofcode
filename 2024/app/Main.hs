b0ba8fd06f59d2879c01678ee2c487a48a5fdd9b
Jeff Arenson <arensonjr@gmail.com>
2024-12-10 10:41:20 -0700

    Incorrect solution to day 6 part 2

--- 2024/app/Main.hs
+++ 2024/app/Main.hs
@@ -1,4 +1,4 @@
-{-# LANGUAGE ScopedTypeVariables #-}
+{-# LANGUAGE ScopedTypeVariables, TypeSynonymInstances, FlexibleInstances #-}
 
 module Main where
 
@@ -6,7 +6,7 @@ import Control.Exception (IOException, catch)
 import Data.List (sort, findIndices, elemIndices, elemIndex, sortBy, isInfixOf, transpose)
 import Data.List.Split (splitOn)
 import Data.Ord (Down(Down), comparing, Ordering(LT, EQ, GT))
-import Data.Maybe (catMaybes, isJust, isNothing)
+import Data.Maybe (catMaybes, isJust, isNothing, listToMaybe)
 import Debug.Trace (trace)
 import System.Environment (getArgs)
 import System.IO (isEOF)
@@ -21,9 +21,9 @@ import qualified Data.HashSet as HS
 import qualified Data.DiGraph as G
 
 -- I wonder if I'd use this...
-(%) :: (a ->b) -> (b -> c) -> a -> c
-(%) = flip (.)
-infixr 9 %
+(&) :: (a ->b) -> (b -> c) -> a -> c
+(&) = flip (.)
+infixl 9 &
 
 data Day = Day1 | Day2 | Day3 | Day4 | Day5
          | Day6 | Day7 | Day8 | Day9 | Day10
@@ -33,6 +33,20 @@ data Day = Day1 | Day2 | Day3 | Day4 | Day5
          deriving (Read, Show)
 
 data Part = Part1 | Part2 deriving (Read, Show)
+
+type Coord = (Int, Int)
+instance Num Coord where
+    (x, y) + (a, b) = (x+a, y+b)
+    (x, y) * (a, b) = (x*a, y*b)
+    negate (x, y) = (-x, -y)
+    fromInteger x = (fromInteger x, fromInteger x)
+    signum (x, y) = (signum x, signum y)
+    abs (x, y) = (abs x, abs y)
+taxiDistance :: Coord -> Coord -> Int
+taxiDistance src dest = dx + dy
+    where (dx, dy) = abs (dest - src)
+
+
 
 readDay :: String -> Day
 readDay = read
@@ -69,22 +83,17 @@ runDay Day25 = day25
 
 step :: (Show a) => String -> a -> a
 -- DEBUG:
-step tag val = trace (tag ++ ": {{ " ++ show val ++ " }}") val 
+-- step tag val = trace (tag ++ ": {{ " ++ show val ++ " }}") val 
 -- NO DEBUG:
--- step tag val = val
+step tag val = val
 
 getLines :: IO [[String]]
 getLines = do
     done <- isEOF
     if done then return [] else do
         line <- getLine `catch` \(e :: IOException) -> return []
-    --    case line of
-    --        [] -> return []
-    --        ls -> fmap ((:) . parseLine $ line) getLines
         (:) (parseLine line) <$> getLines
 
-parseLine :: String -> [String]
-parseLine [] = []
 parseLine ls = filter (not . null) . words $ ls
 
 main :: IO ()
@@ -188,7 +197,7 @@ parseDay5 input = do
     let [edges, lists] = step "split lits" [map head ls | ls <- splitOn [[]] input]
     (MM.fromList (map parseDay5Edge edges), map parseDay5List lists)
 parseDay5Edge = (\[x, y] -> (x, y)) . map read . splitOn "|"
-parseDay5List = map read . splitOn "," . step "lists"
+parseDay5List = step "lists" & splitOn "," & map read
 checkDay5List graph ls = (not . or) [elemIndex x ls > elemIndex y ls | (x, y) <- MM.toList graph, x `elem` ls, y `elem` ls]
 graphOrdering graph x y
   | y `elem` MM.lookup x graph = LT
@@ -214,8 +223,98 @@ day5 Part2 nums = do
 
 
 day6 :: Part -> [[String]] -> Int
-day6 Part1 input = 0
-day6 Part2 input = 0
+
+rightTurn :: (Int, Int) -> (Int, Int)
+rightTurn (0, y) = (negate y, 0)
+rightTurn (x, 0) = (0, x)
+
+between :: Int -> Int -> Int -> Bool
+between bound1 mid bound2
+    | bound1 < mid = mid < bound2
+    | bound1 > mid = mid > bound2
+    | otherwise = False
+
+extendOne :: Int -> Int -> Int -> Int -> [Int]
+extendOne x 0 _ _ = [x]
+extendOne x dx bound1 bound2 = [x, x+dx .. boundX-dx]
+    where boundX = if dx < 0 then min bound1 bound2 else max bound1 bound2
+extend :: Coord -> Coord -> Coord -> Coord -> [Coord]
+extend (x, y) (vecX, vecY) (startX, startY) (endX, endY) = [
+    (x', y') | x' <- extendOne x vecX startX endX,
+               y' <- extendOne y vecY startY endY]
+
+parseDay6 :: [[String]] -> (Coord, [Coord], Coord)
+parseDay6 lines = do
+    let grid = map head lines
+    let size = (length (head grid), length grid)
+    let withCoords = [(x, y, val) | (y, row) <- zip [0..] grid, (x, val) <- zip [0..] row]
+    let startPos = head [(x, y) | (x, y, val) <- withCoords, val == '^']
+    let boxes = [(x, y) | (x, y, val) <- withCoords, val == '#']
+    (startPos, boxes, size)
+
+nextBox size boxes pos vector = do
+    let candidates = step "candidates" $ extend pos vector (fromInteger (-1)) size
+    let found = step "found" $ L.intersect candidates boxes
+    let nextFound = step "nextFound" $ listToMaybe $ sortBy (comparing (taxiDistance pos)) found
+    case nextFound of
+       Nothing -> step "next found nothing" Nothing
+       Just next -> do
+            let newBoxes = step "newBoxes" $ extend pos vector (pos + vector) (next - vector)
+            let newVector = step "newVector" $ rightTurn vector
+            Just (next - vector, S.fromList newBoxes, newVector)
+
+walk visited boxes pos size vector = do
+    case nextBox size boxes pos vector of
+        Just (nextPos, newVisited, newVector) -> walk (S.union visited newVisited) boxes nextPos size newVector
+        Nothing -> S.union visited . S.fromList $ extend pos vector (fromInteger (-1)) size
+
+day6 Part1 input = do
+    let (startPos, boxes, size) = step "parsed" $ parseDay6 input
+    let vector = (0, -1) -- up
+    let visited = step "final visited" $ walk (S.fromList [startPos]) boxes startPos size vector
+    length visited
+
+day6 Part2 input = do
+    let (startPos, boxes, size) = step "parsed" $ parseDay6 input
+    let vector = (0, -1) -- up
+    let corners = step "final corners" $ walkCorners [] boxes startPos size vector
+    let obstacles = step "obstacles" $ setObstacles boxes (startPos:reverse corners)
+    (length . S.fromList) obstacles
+
+nextBoxCorners size boxes pos vector = do
+    let candidates = step "candidates" $ extend pos vector (fromInteger (-1)) size
+    let found = step "found" $ L.intersect candidates boxes
+    let nextFound = step "nextFound" $ listToMaybe $ sortBy (comparing (taxiDistance pos)) found
+    case nextFound of
+       Nothing -> step "next found nothing" Nothing
+       Just next -> do
+            let newVector = step "newVector" $ rightTurn vector
+            Just (next - vector, newVector)
+walkCorners corners boxes pos size vector = do
+    case nextBoxCorners size boxes pos vector of
+        Just (nextPos, newVector) -> walkCorners (nextPos:corners) boxes nextPos size newVector
+        Nothing -> corners
+
+-- TODO: This doesn't work and not sure why. Probably time to just go brute-force and try each new box location to see if I
+-- re-cross the same corner before exiting the grid.
+setObstacles :: [Coord] -> [Coord] -> [Coord]
+setObstacles boxes (x:y:z:rest) = do
+    -- TODO: Technically, I should account for the obstacles being 1 step farther
+    -- than where the guard stops, but I don't think it'll matter...
+    -- TODO: Need to handle overshoots: the actual obstacle was sooner than the theoretical obstacle would have to be
+    -- TODO: Probably need to handle when the theoretical obstacle would have blocked a traversal earlier in the path
+    let obstacle = step "newObstacle" $ findObstacle (step "x" x) (step "y" y) (step "z" z)
+    let onTheWay = step "onTheWay" $ extend z (signum (obstacle - z)) z obstacle
+    let remainder = step "remainder" $ setObstacles boxes (y:z:rest)
+    if null (step "intersected" $ L.intersect onTheWay boxes)
+        then obstacle : remainder
+        else remainder
+setObstacles _ tooFewCorners = []
+
+findObstacle (x1, y1) (x2, y2) (x3, y3)
+    | x1 == x2 = (x3, y1)
+    | y1 == y2 = (x1, y3)
+    | otherwise = error "WAT"
 
 day7  part nums = 0
 day8  part nums = 0
