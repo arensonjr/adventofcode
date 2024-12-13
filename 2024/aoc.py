16bf805b36e0c6f8e67aaea08be4c71452738aeb
Jeff Arenson <arensonjr@gmail.com>
2024-12-13 08:38:39 -0700

    Day 13 done, gave up on day 12 pt 2, added networkx module setup

--- 2024/aoc.py
+++ 2024/aoc.py
@@ -1,14 +1,132 @@
-import sys
-import os
-import functools
 import collections
-import time
 import datetime
+import functools
+import networkx
+import os
+import re
+import sys
+import time
 
 ######################################################################
 
-def day12_part1(lines):
-    pass
+def day13_part1(lines): return day13(lines, 0)
+def day13_part2(lines): return day13(lines, 10000000000000)
+def day13(lines, prize_delta):
+    # Parse
+    games = []
+    i = 0
+    def _parse(splitline): return (int(splitline[1][2:-1]), int(splitline[2][2:]))
+    while i < len(lines):
+        games.append((
+            _parse(lines[i].split()[1:]),
+             _parse(lines[i+1].split()[1:]),
+             tuple(map(lambda x: x + prize_delta, _parse(lines[i+2].split())))
+        ))
+        i += 4
+
+    debug(f'{games=}')
+
+    # Solve system of equations
+    total_tokens = 0
+    for game in games:
+        ((ax, ay), (bx, by), (prizex, prizey)) = game
+        # let n = num A joysticks, k = num B joysticks
+        # n * ax + k * bx = prizex
+        #   -> k = (-ax/bx)n + (prizex/bx)
+        # n * ay + k * by = prizey
+        #   -> k = (-ay/by)n + (prizey/by)
+        # (-ax/bx)n + (prizex/bx) = (-ay/by)n + (prizey/by)
+        # (ay/by - ax/bx) n = (prizey/by - prizex/bx)
+        # n = (prizey/by - prizex/bx) / (ay/by - ax/bx)
+        # ergo
+        # k = (prizey/ay - prizex/ax) / (by/ay - bx/ax)
+        n = ((prizey / by) - (prizex / bx)) / ((ay / by) - (ax / bx))
+        int_n = round(n)
+        k = ((prizey / ay) - (prizex / ax)) / ((by / ay) - (bx / ax))
+        int_k = round(k)
+        if abs(int_n - n) < 0.0001 and abs(int_k - k) < 0.0001:
+            total_tokens += 3 * int_n + int_k
+
+    return total_tokens
+
+
+def neighbors(x, y):
+    return [(x-1, y), (x+1, y), (x, y-1), (x, y+1)]
+
+def split_crops(grid):
+    debug(f'Splitting crops: {grid=}')
+
+    height = len(grid)
+    width = len(grid[0])
+    graph = networkx.Graph()
+    for x in range(width):
+        for y in range(height):
+            graph.add_node((x, y))
+            for (nx, ny) in neighbors(x, y):
+                if 0 <= nx < width and 0 <= ny < height and grid[y][x] == grid[ny][nx]:
+                    graph.add_edge((x, y), (nx, ny))
+
+    return list(networkx.connected_components(graph))
+
+def perimeter(region):
+    debug(f'Finding perimeter of {region=}')
+    # The perimeter is equal to the number of neighbors outside the region (non-unique, since we can border it on more than one side).
+    return len([neighbor for (x, y) in region for neighbor in neighbors(x, y) if neighbor not in region])
+
+def num_sides(region):
+    debug(f'Finding number of sides of {region=}')
+
+    # Trying a different approach:
+    remaining_neighbors = [neighbor for (x, y) in region for neighbor in neighbors(x, y) if neighbor not in region]
+    all_neighbors = set(remaining_neighbors)
+    debug(f'  | Found starting neighbors {sorted(all_neighbors)}')
+    sides = 0
+
+    # Walk along each side, removing neighbors, until we've fully explored each side
+    while remaining_neighbors:
+        sides += 1
+        top_left = min(remaining_neighbors)
+        debug(f'  | exploring from {top_left=}')
+        (x, y) = top_left
+
+        # Figure out which direction to walk for this side (or none if singleton edge is all that remains)
+        top_left_neighbors = [pos for pos in neighbors(x, y) if pos in remaining_neighbors]
+        if not top_left_neighbors:
+            debug(f'  | ({(x, y)} is a singleton side, continuing)')
+            remaining_neighbors.remove(top_left)
+            continue
+        else:
+            (x2, y2) = top_left_neighbors[0]
+            (dx, dy) = (x2-x, y2-y)
+
+        # Walk all neighbors along this side
+        while True:
+            debug(f'  |  --> trying to remove {(x,y)} from {sorted(remaining_neighbors)}...')
+            remaining_neighbors.remove((x, y))
+            if (next := (x+dx, y+dy)) in remaining_neighbors:
+                (x, y) = next
+                debug(f'  | ... walked to {(x, y)=} along vector {(dx, dy)=}...')
+            else:
+                debug(f'  | cant keep going: edge is complete!')
+                break
+    return sides
+
+def day12_part1(lines): return day12(lines, perimeter)
+# TODO: Not sure why part 2 breaks; works on all the tests :/
+def day12_part2(lines): return day12(lines, num_sides)
+def day12(lines, perim_func):
+    # Parse
+    height = len(lines)
+    width = len(lines[0])
+    grid = {(x, y): lines[y][x] for x in range(width) for y in range(height)}
+
+    # Compute
+    # regions = split_crops(grid)
+    regions = split_crops(lines)
+    region_summaries = [(len(region), perim_func(region), grid[region.pop()]) for region in regions]
+    debug(f'{region_summaries=}')
+    return sum([(area * perim) for (area, perim, letter) in region_summaries])
+
 
 def day11_part1(lines): return day11(lines, 25)
 def day11_part2(lines): return day11(lines, 75)
